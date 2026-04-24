dc35ceed5a60558f22eccd87a9df2ccdfe66d661
Gautham Venkatasubramanian <ahgamut@gmail.com>
2026-04-24 11:33:36 -0500

    use OO for estimators
    easier to track errors

--- examples/compare_sim_points.py
+++ examples/compare_sim_points.py
@@ -6,28 +6,15 @@ import numpy as np
 import pandas as pd
 import time
 
-# https://github.com/ahgamut/cliquematch/tree/devel
-import cliquematch
-
-#
-from rts_align import construct_graph_2d
-from rts_align import KabschEstimate
-from rts_align import find_clique
-from rts_align.clq import get_clique
-
-# https://github.com/yangjiaolong/Go-ICP
-# via https://github.com/aalavandhaann/go-icp_cython
-# 4568dd976fc5a63246835edbc748f35bc483f409
-import py_goicp
-
-# https://github.com/ariarobotics/clipperp
-# a524943411bf6635219ab510864c81aa1b6a0c7a
-# (patch headers in python bindings)
-import clipperpluspy
-
-# https://github.com/MIT-SPARK/TEASER-plusplus
-# f91cfdb7baed951a3607257bd31f3f6694773497
-import teaserpp_python
+from estim_2d import (
+    GoICP,
+    GoICPScaled,
+    TEASER,
+    TEASER_NoShuf,
+    CLIPPER,
+    RTS,
+    RTSHeuristic,
+)
 
 
 def generate_points(n, md=10):
@@ -49,280 +36,33 @@ def rigid_form(pts, theta, d):
     return np.matmul(pts, rotmat) + d
 
 
-def ek_wrapper(_keys):
-
-    def error_wrapper(foo):
-
-        def wrapped(*a1, **a2):
-            sol = dict()
-            for k in _keys:
-                sol[k] = None
-            try:
-                s2 = foo(*a1, **a2)
-                for k, v in s2.items():
-                    sol[k] = v
-            except Exception as e:
-                print(foo, "failed:", type(e), e, file=sys.stderr)
-            return sol
-
-        return wrapped
-
-    return error_wrapper
+#####
 
 
-###
+def make_estims(d):
+    result = dict()
 
+    outlier_frac = (1 + d.num_extra) / (1 + d.num_K + d.num_extra)
 
-def make_p3d_list(arr, msize):
-    N = len(arr)
-    res = []
-    for i in range(N):
-        x, y = (arr[i, :]) / (1.2 * msize)  # all values inside [-1, 1]
-        res.append(py_goicp.POINT3D(x, y, 0.0))
-    return N, res
-
-@ek_wrapper(
-    _keys=[
-        "goicp_success",
-        "goicp_zoom",
-        "goicp_theta",
-        "goicp_dx",
-        "goicp_dy",
-        "goicp_time",
+    estim_objs = [
+        GoICP(outlier_frac=outlier_frac),
+        GoICPScaled(outlier_frac=outlier_frac),
+        TEASER(noise_range=d.noise_add),
+        TEASER_NoShuf(noise_range=d.noise_add),
+        CLIPPER(delta=d.delta, epsilon=d.epsilon),
+        RTS(delta=d.delta, epsilon=d.epsilon),
+        RTSHeuristic(delta=d.delta, epsilon=d.epsilon),
     ]
-)
-def goicp_estim(q_pts, k_pts, outlier_frac):
-    msize = max(np.max(np.abs(q_pts)), np.max(np.abs(k_pts)))
-    Nq, qp = make_p3d_list(q_pts, msize)
-    Nk, kp = make_p3d_list(k_pts, msize)
 
-    mod = py_goicp.GoICP()
-    mod.MSEThresh = 0.0001
-    mod.trimFraction = outlier_frac
-    start_time = time.time()
-
-    mod.loadModelAndData(Nk, kp, Nq, qp)
-    mod.setDTSizeAndFactor(300, 2.0)
-    mod.BuildDT()
-    mod.Register()
-
-    end_time = time.time()
-    scale = np.linalg.det(mod.optimalRotation())
-    rotmat = np.array(mod.optimalRotation())[:2, :2]
-    trans0 = np.array(mod.optimalTranslation())[:2] * 1.2 * msize
-    transl = -np.matmul(rotmat.T, trans0)
-
-    sol = dict()
-    sol["goicp_success"] = True
-    sol["goicp_zoom"] = scale
-    sol["goicp_theta"] = np.arctan2(rotmat[0, 1], rotmat[0, 0])
-    sol["goicp_theta2"] = np.arctan2(rotmat.T[0, 1], rotmat.T[0, 0])
-    sol["goicp_dx"] = transl[0]
-    sol["goicp_dy"] = transl[1]
-    sol["goicp_time"] = float(end_time - start_time)
-    return sol
-
-
-@ek_wrapper(
-    _keys=[
-        "goicp-scaled_success",
-        "goicp-scaled_zoom",
-        "goicp-scaled_theta",
-        "goicp-scaled_dx",
-        "goicp-scaled_dy",
-        "goicp-scaled_time",
-    ]
-)
-def goicp_estim2(q_pts, k_pts, outlier_frac, scale):
-    msize = max(np.max(np.abs(q_pts)), np.max(np.abs(k_pts * scale)))
-    Nq, qp = make_p3d_list(q_pts, msize)
-    Nk, kp = make_p3d_list(k_pts * scale, msize)
-
-    mod = py_goicp.GoICP()
-    mod.MSEThresh = 0.0001
-    mod.trimFraction = 0.5
-    start_time = time.time()
-
-    mod.loadModelAndData(Nk, kp, Nq, qp)
-    mod.setDTSizeAndFactor(300, 2.0)
-    mod.BuildDT()
-    mod.Register()
-
-    end_time = time.time()
-    rotmat = np.array(mod.optimalRotation())[:2, :2]
-    trans0 = np.array(mod.optimalTranslation())[:2] * 1.2 * msize
-    transl = -np.matmul(rotmat.T, trans0)
-
-    sol = dict()
-    sol["goicp-scaled_success"] = True
-    sol["goicp-scaled_zoom"] = scale
-    sol["goicp-scaled_theta"] = np.arctan2(rotmat[0, 1], rotmat[0, 0])
-    sol["goicp-scaled_theta2"] = np.arctan2(rotmat.T[0, 1], rotmat.T[0, 0])
-    sol["goicp-scaled_dx"] = transl[0]
-    sol["goicp-scaled_dy"] = transl[1]
-    sol["goicp-scaled_time"] = float(end_time - start_time)
-    return sol
-
-
-####
-
-
-@ek_wrapper(
-    _keys=[
-        "clipperp_success",
-        "clipperp_zoom",
-        "clipperp_theta",
-        "clipperp_dx",
-        "clipperp_dy",
-        "clipperp_time",
-    ]
-)
-def clipperp_estim(q_pts, k_pts, delta, epsilon):
-    delta = delta * np.pi / 180.0
-    qlen = len(q_pts)
-    klen = len(k_pts)
-
-    # timer
-    start_time = time.time()
-    adjmat = construct_graph_2d(
-        q_pts, k_pts, delta=delta, epsilon=epsilon, max_ratio=10, min_ratio=0.1
-    )
-
-    # timer
-    mid_time = time.time()
-
-    adjmat = np.int32(adjmat != 0)
-    adjmat = adjmat | adjmat.T
-    np.fill_diagonal(adjmat, 0)
-    clique_size, clique, certificate = clipperpluspy.clipperplus_clique(adjmat)
-
-    # timer
-    end_time = time.time()
-
-    clique = np.array(clique, dtype=np.int32)
-    qc = q_pts[clique // len(k_pts), :]
-    kc = k_pts[clique % len(k_pts), :]
-    tm = {"start": start_time, "mid": mid_time, "end": end_time}
-
-    tform = KabschEstimate(kc, qc)
-    transl_est = tform.coefs[0, :]
-    theta_est = np.arctan2(tform.coefs[1, 1], tform.coefs[1, 0])
-    zoom_est = np.sqrt(np.linalg.det(tform.coefs[1:, :]))
-
-    sol = dict()
-    sol["clipperp_success"] = True
-    sol["clipperp_zoom"] = zoom_est
-    sol["clipperp_theta"] = theta_est
-    sol["clipperp_dx"] = transl_est[0]
-    sol["clipperp_dy"] = transl_est[1]
-    sol["clipperp_time"] = float(tm["end"] - tm["start"])
-    sol["clipperp_time-clq"] = float(tm["end"] - tm["mid"])
-    sol["clipperp_time-graph"] = float(tm["mid"] - tm["start"])
-    return sol
+    for e in estim_objs:
+        result[e.__estim_name__] = e
+    return result
 
 
 #####
 
 
-@ek_wrapper(
-    _keys=[
-        "teaser_success",
-        "teaser_zoom",
-        "teaser_theta",
-        "teaser_dx",
-        "teaser_dy",
-        "teaser_time",
-    ]
-)
-def teaser_estim(q_pts, k_pts, noise_range):
-    dst = np.column_stack([q_pts, 0 * np.ones(len(q_pts))]).T
-    src = np.column_stack([k_pts, 0 * np.ones(len(k_pts))]).T
-
-    solver_params = teaserpp_python.RobustRegistrationSolver.Params()
-    solver_params.cbar2 = 1
-    solver_params.noise_bound = 5 * noise_range
-    solver_params.estimate_scaling = True
-    solver_params.rotation_estimation_algorithm = (
-        teaserpp_python.RobustRegistrationSolver.ROTATION_ESTIMATION_ALGORITHM.GNC_TLS
-    )
-    solver_params.rotation_tim_graph = (
-        teaserpp_python.RobustRegistrationSolver.INLIER_GRAPH_FORMULATION.COMPLETE
-    )
-    solver_params.inlier_selection_mode = (
-        teaserpp_python.RobustRegistrationSolver.INLIER_SELECTION_MODE.PMC_EXACT
-    )
-    solver_params.rotation_gnc_factor = 1.4
-    solver_params.rotation_max_iterations = 1000
-    solver_params.rotation_cost_threshold = 1e-12
-    solver_params.kcore_heuristic_threshold = 1.0
-
-    solver = teaserpp_python.RobustRegistrationSolver(solver_params)
-    start = time.time()
-    solver.solve(src, dst)
-    end = time.time()
-
-    solution = solver.getSolution()
-
-    sol = dict()
-    sol["teaser_success"] = True
-    sol["teaser_zoom"] = solution.scale
-    rotmat = solution.rotation[:2, :2]
-    sol["teaser_theta"] = np.arctan2(rotmat[1, 0], rotmat[1, 1])
-    sol["teaser_theta2"] = np.arctan2(rotmat.T[1, 0], rotmat.T[1, 1])
-    sol["teaser_dx"] = solution.translation[0]
-    sol["teaser_dy"] = solution.translation[1]
-    sol["teaser_time"] = float(end - start)
-    return sol
-
-
-def teaser2_estim(q_pts, k_pts, noise_range):
-    sol0 = teaser_estim(q_pts, k_pts, noise_range)
-    sol = dict()
-    for k, v in sol0.items():
-        k2 = k.replace("teaser", "teaser-noshuf")
-        sol[k2] = v
-    return sol
-
-
-#####
-
-
-@ek_wrapper(
-    _keys=[
-        "rts_success",
-        "rts_zoom",
-        "rts_theta",
-        "rts_dx",
-        "rts_dy",
-        "rts_time",
-    ]
-)
-def rts_estim(q_pts, k_pts, delta, epsilon):
-    # find corresponding points and visualize
-    sol0 = find_clique(q_pts, k_pts, delta=delta, epsilon=epsilon)
-    qc, kc, tm = sol0["qc"], sol0["kc"], sol0["tm"]
-    tform = KabschEstimate(kc, qc)
-
-    transl_est = tform.coefs[0, :]
-    theta_est = np.arctan2(tform.coefs[1, 1], tform.coefs[1, 0])
-    zoom_est = np.sqrt(np.linalg.det(tform.coefs[1:, :]))
-
-    sol = dict()
-    sol["rts_success"] = True
-    sol["rts_zoom"] = zoom_est
-    sol["rts_theta"] = theta_est
-    sol["rts_dx"] = transl_est[0]
-    sol["rts_dy"] = transl_est[1]
-    sol["rts_time"] = float(tm["end"] - tm["start"])
-    sol["rts_time-clq"] = float(tm["end"] - tm["mid"])
-    sol["rts_time-graph"] = float(tm["mid"] - tm["start"])
-    return sol
-
-
-#####
-
-
-def attempt(num_K, num_extra=0, noise_range=1, delta=0.1, epsilon=0.1):
+def attempt(num_K, estims, num_extra=0, noise_range=1, delta=0.1, epsilon=0.1):
     k_pts = generate_points(num_K + num_extra)
 
     # randomly select R/T/S
@@ -341,19 +81,19 @@ def attempt(num_K, num_extra=0, noise_range=1, delta=0.1, epsilon=0.1):
     k_pts = k_pts
 
     # mappings that require correspondence
-    sol_teaser2 = teaser2_estim(q_pts, k_pts, noise_range)
+    sol_teaser2 = estims["teaser-noshuf"](q_pts, k_pts, noise_range=noise_range)
 
     # shuffle the points
     np.random.shuffle(q_pts)
     np.random.shuffle(k_pts)
 
     # mappings that don't require correspondence
-    sol_rts = rts_estim(q_pts, k_pts, delta, epsilon)
-    sol_clipperp = clipperp_estim(q_pts, k_pts, delta, epsilon)
-    sol_teaser1 = teaser_estim(q_pts, k_pts, noise_range)
-    outlier_frac = (1 + num_extra) / (1 + len(k_pts))
-    sol_goicp1 = goicp_estim(q_pts, k_pts, outlier_frac)
-    sol_goicp2 = goicp_estim2(q_pts, k_pts, outlier_frac, zoom)
+    sol_rts1 = estims["rts"](q_pts, k_pts)
+    sol_rts2 = estims["rts-heuristic"](q_pts, k_pts)
+    sol_clipperp = estims["clipperp"](q_pts, k_pts, delta, epsilon)
+    sol_teaser1 = estims["teaser"](q_pts, k_pts, noise_range=noise_range)
+    sol_goicp1 = estims["goicp"](q_pts, k_pts)
+    sol_goicp2 = estims["goicp-scaled"](q_pts, k_pts, scale=zoom)
 
     # add entries
     res = dict()
@@ -367,10 +107,11 @@ def attempt(num_K, num_extra=0, noise_range=1, delta=0.1, epsilon=0.1):
     res["dx"] = translation[0]
     res["dy"] = translation[1]
 
+    res.update(sol_clipperp)
+    res.update(sol_rts1)
+    res.update(sol_rts2)
     res.update(sol_teaser1)
     res.update(sol_teaser2)
-    res.update(sol_rts)
-    res.update(sol_clipperp)
     res.update(sol_goicp1)
     res.update(sol_goicp2)
     print(res, file=sys.stderr)
@@ -408,12 +149,13 @@ def main():
     )
 
     d = parser.parse_args()
+    estims = make_estims(d)
     result = []
     i = 0
     while i < d.simulations:
         try:
             print(i, file=sys.stderr)
-            r = attempt(d.num_K, d.num_extra, d.noise_add, d.delta, d.epsilon)
+            r = attempt(d.num_K, estims, d.num_extra, d.noise_add, d.delta, d.epsilon)
             result.append(r)
         except Exception as e:
             print("attempt failure", i, e, file=sys.stderr)
---
+++ examples/estim_2d.py
@@ -0,0 +1,329 @@
+import sys
+import argparse
+import math
+import itertools
+import numpy as np
+import pandas as pd
+import time
+import traceback
+
+# https://github.com/ahgamut/cliquematch/tree/devel
+import cliquematch
+
+#
+from rts_align import construct_graph_2d
+from rts_align import KabschEstimate
+from rts_align import find_clique
+from rts_align.clq import get_clique
+
+# https://github.com/yangjiaolong/Go-ICP
+# via https://github.com/aalavandhaann/go-icp_cython
+# 4568dd976fc5a63246835edbc748f35bc483f409
+import py_goicp
+
+# https://github.com/ariarobotics/clipperp
+# a524943411bf6635219ab510864c81aa1b6a0c7a
+# (patch headers in python bindings)
+import clipperpluspy
+
+# https://github.com/MIT-SPARK/TEASER-plusplus
+# f91cfdb7baed951a3607257bd31f3f6694773497
+import teaserpp_python
+
+
+class Base2DEstimator:
+    __estim_name__ = None
+
+    __default_params__ = set(["zoom", "theta", "dx", "dy"])
+    __aux_params__ = set(["success", "time"])
+
+    def __init__(self, *args, **kwargs):
+        params = set(kwargs.get("params", [])) | self.__default_params__
+        params = params - self.__aux_params__
+        e = self.__estim_name__
+        self._dummy_result = dict()
+        self._dummy_params = set()
+        for k in params:
+            self._dummy_result[f"{e}_{k}"] = 0.0
+            self._dummy_params.add(f"{e}_{k}")
+        self._dummy_result[f"{e}_success"] = False
+
+    def _call_impl(self, q_pts, k_pts, *args, **kwargs):
+        raise NotImplementedError("subclass Base2DEstimator")
+
+    def __call__(self, q_pts, k_pts, *args, **kwargs):
+        result = dict(**self._dummy_result)
+        e = self.__estim_name__
+        try:
+            start = time.time()
+            subres = self._call_impl(q_pts, k_pts, *args, **kwargs)
+            end = time.time()
+            for k in self._dummy_params:
+                result[k] = subres[k]
+            result[f"{e}_success"] = True
+            result[f"{e}_time"] = end - start
+        except Exception as err:
+            msg = traceback.format_exc()
+            print(f"estimation with {self.__estim_name__} failed with", msg)
+            result[f"{e}_success"] = False
+            result[f"{e}_time"] = 0.0
+        return result
+
+
+######
+
+
+def make_p3d_list(arr, msize):
+    N = len(arr)
+    res = []
+    for i in range(N):
+        x, y = (arr[i, :]) / (1.2 * msize)  # all values inside [-1, 1]
+        res.append(py_goicp.POINT3D(x, y, 0.0))
+    return N, res
+
+
+class GoICP(Base2DEstimator):
+    __estim_name__ = "goicp"
+
+    def __init__(self, outlier_frac, mse_thresh=0.0001):
+        super().__init__()
+        self.outlier_frac = outlier_frac
+        self.mse_thresh = mse_thresh
+
+    def _call_impl(self, q_pts, k_pts, *args, **kwargs):
+        msize = max(np.max(np.abs(q_pts)), np.max(np.abs(k_pts)))
+        Nq, qp = make_p3d_list(q_pts, msize)
+        Nk, kp = make_p3d_list(k_pts, msize)
+
+        mod = py_goicp.GoICP()
+        mod.MSEThresh = self.mse_thresh
+        mod.trimFraction = self.outlier_frac
+
+        mod.loadModelAndData(Nk, kp, Nq, qp)
+        mod.setDTSizeAndFactor(300, 2.0)
+        mod.BuildDT()
+        mod.Register()
+
+        scale = np.linalg.det(mod.optimalRotation())
+        rotmat = np.array(mod.optimalRotation())[:2, :2]
+        trans0 = np.array(mod.optimalTranslation())[:2] * 1.2 * msize
+        transl = -np.matmul(rotmat.T, trans0)
+
+        sol = dict()
+        e = self.__estim_name__
+        sol[f"{e}_zoom"] = scale
+        sol[f"{e}_theta"] = np.arctan2(rotmat[0, 1], rotmat[0, 0])
+        sol[f"{e}_dx"] = transl[0]
+        sol[f"{e}_dy"] = transl[1]
+        return sol
+
+
+class GoICPScaled(Base2DEstimator):
+    __estim_name__ = "goicp-scaled"
+
+    def __init__(self, outlier_frac, mse_thresh=0.0001):
+        super().__init__()
+        self.outlier_frac = outlier_frac
+        self.mse_thresh = mse_thresh
+
+    def _call_impl(self, q_pts, k_pts, *args, **kwargs):
+        scale = kwargs.get("scale", 1.0)
+        msize = max(np.max(np.abs(q_pts)), np.max(np.abs(k_pts * scale)))
+        Nq, qp = make_p3d_list(q_pts, msize)
+        Nk, kp = make_p3d_list(k_pts * scale, msize)
+
+        mod = py_goicp.GoICP()
+        mod.MSEThresh = self.mse_thresh
+        mod.trimFraction = self.outlier_frac
+
+        mod.loadModelAndData(Nk, kp, Nq, qp)
+        mod.setDTSizeAndFactor(300, 2.0)
+        mod.BuildDT()
+        mod.Register()
+
+        scale = np.linalg.det(mod.optimalRotation())
+        rotmat = np.array(mod.optimalRotation())[:2, :2]
+        trans0 = np.array(mod.optimalTranslation())[:2] * 1.2 * msize
+        transl = -np.matmul(rotmat.T, trans0)
+
+        sol = dict()
+        e = self.__estim_name__
+        sol[f"{e}_zoom"] = kwargs.get("scale", 1.0)
+        sol[f"{e}_theta"] = np.arctan2(rotmat[0, 1], rotmat[0, 0])
+        sol[f"{e}_dx"] = transl[0]
+        sol[f"{e}_dy"] = transl[1]
+        return sol
+
+
+class TEASER(Base2DEstimator):
+    __estim_name__ = "teaser"
+
+    def __init__(self, noise_range):
+        super().__init__()
+        self.noise_range = noise_range
+
+    def _call_impl(self, q_pts, k_pts, *args, **kwargs):
+        dst = np.column_stack([q_pts, 0 * np.ones(len(q_pts))]).T
+        src = np.column_stack([k_pts, 0 * np.ones(len(k_pts))]).T
+
+        solver_params = teaserpp_python.RobustRegistrationSolver.Params()
+        solver_params.cbar2 = 1
+        solver_params.noise_bound = 5 * self.noise_range
+        solver_params.estimate_scaling = True
+        solver_params.rotation_estimation_algorithm = (
+            teaserpp_python.RobustRegistrationSolver.ROTATION_ESTIMATION_ALGORITHM.GNC_TLS
+        )
+        solver_params.rotation_tim_graph = (
+            teaserpp_python.RobustRegistrationSolver.INLIER_GRAPH_FORMULATION.COMPLETE
+        )
+        solver_params.inlier_selection_mode = (
+            teaserpp_python.RobustRegistrationSolver.INLIER_SELECTION_MODE.PMC_EXACT
+        )
+        solver_params.rotation_gnc_factor = 1.4
+        solver_params.rotation_max_iterations = 1000
+        solver_params.rotation_cost_threshold = 1e-12
+        solver_params.kcore_heuristic_threshold = 1.0
+
+        solver = teaserpp_python.RobustRegistrationSolver(solver_params)
+        solver.solve(src, dst)
+        solution = solver.getSolution()
+        rotmat = solution.rotation[:2, :2]
+
+        e = self.__estim_name__
+        sol = dict()
+        sol[f"{e}_zoom"] = solution.scale
+        sol[f"{e}_theta"] = np.arctan2(rotmat[1, 0], rotmat[1, 1])
+        sol[f"{e}_dx"] = solution.translation[0]
+        sol[f"{e}_dy"] = solution.translation[1]
+        return sol
+
+
+class TEASER_NoShuf(TEASER):
+    __estim_name__ = "teaser-noshuf"
+
+
+class CLIPPER(Base2DEstimator):
+    __estim_name__ = "clipperp"
+
+    def __init__(self, delta, epsilon, min_ratio=0.1, max_ratio=10.0):
+        super().__init__(params=["time-clq", "time-graph"])
+        self.delta = delta
+        self.epsilon = epsilon
+        self.min_ratio = min_ratio
+        self.max_ratio = max_ratio
+
+    def _call_impl(self, q_pts, k_pts, *args, **kwargs):
+        qlen = len(q_pts)
+        klen = len(k_pts)
+
+        # timer
+        start_time = time.time()
+        adjmat = construct_graph_2d(
+            q_pts,
+            k_pts,
+            delta=self.delta,
+            epsilon=self.epsilon,
+            min_ratio=self.min_ratio,
+            max_ratio=self.max_ratio,
+        )
+
+        # timer
+        mid_time = time.time()
+
+        adjmat = np.int32(adjmat != 0)
+        adjmat = adjmat | adjmat.T
+        np.fill_diagonal(adjmat, 0)
+        clique_size, clique, certificate = clipperpluspy.clipperplus_clique(adjmat)
+
+        # timer
+        end_time = time.time()
+
+        clique = np.array(clique, dtype=np.int32)
+        qc = q_pts[clique // len(k_pts), :]
+        kc = k_pts[clique % len(k_pts), :]
+        tm = {"start": start_time, "mid": mid_time, "end": end_time}
+
+        tform = KabschEstimate(kc, qc)
+        transl_est = tform.coefs[0, :]
+        theta_est = np.arctan2(tform.coefs[1, 1], tform.coefs[1, 0])
+        zoom_est = np.sqrt(np.linalg.det(tform.coefs[1:, :]))
+
+        e = self.__estim_name__
+        sol = dict()
+        sol[f"{e}_zoom"] = zoom_est
+        sol[f"{e}_theta"] = theta_est
+        sol[f"{e}_dx"] = transl_est[0]
+        sol[f"{e}_dy"] = transl_est[1]
+        sol[f"{e}_time-clq"] = float(end_time - mid_time)
+        sol[f"{e}_time-graph"] = float(mid_time - start_time)
+        return sol
+
+
+class RTS(Base2DEstimator):
+    __estim_name__ = "rts"
+
+    def __init__(self, delta, epsilon):
+        super().__init__(params=["time-clq", "time-graph"])
+        self.delta = delta
+        self.epsilon = epsilon
+        self.use_heuristic = True
+
+    def _call_impl(self, q_pts, k_pts, *args, **kwargs):
+        sol0 = find_clique(
+            q_pts,
+            k_pts,
+            delta=self.delta,
+            epsilon=self.epsilon,
+            heuristic=self.use_heuristic,
+        )
+        qc, kc, tm = sol0["qc"], sol0["kc"], sol0["tm"]
+        tform = KabschEstimate(kc, qc)
+
+        transl_est = tform.coefs[0, :]
+        theta_est = np.arctan2(tform.coefs[1, 1], tform.coefs[1, 0])
+        zoom_est = np.sqrt(np.linalg.det(tform.coefs[1:, :]))
+
+        e = self.__estim_name__
+        sol = dict()
+        sol[f"{e}_zoom"] = zoom_est
+        sol[f"{e}_theta"] = theta_est
+        sol[f"{e}_dx"] = transl_est[0]
+        sol[f"{e}_dy"] = transl_est[1]
+        sol[f"{e}_time-clq"] = float(tm["end"] - tm["mid"])
+        sol[f"{e}_time-graph"] = float(tm["mid"] - tm["start"])
+        return sol
+
+
+class RTSHeuristic(Base2DEstimator):
+    __estim_name__ = "rts-heuristic"
+
+    def __init__(self, delta, epsilon):
+        super().__init__(params=["time-clq", "time-graph"])
+        self.delta = delta
+        self.epsilon = epsilon
+        self.use_heuristic = False
+
+    def _call_impl(self, q_pts, k_pts, *args, **kwargs):
+        sol0 = find_clique(
+            q_pts,
+            k_pts,
+            delta=self.delta,
+            epsilon=self.epsilon,
+            heuristic=self.use_heuristic,
+        )
+        qc, kc, tm = sol0["qc"], sol0["kc"], sol0["tm"]
+        tform = KabschEstimate(kc, qc)
+
+        transl_est = tform.coefs[0, :]
+        theta_est = np.arctan2(tform.coefs[1, 1], tform.coefs[1, 0])
+        zoom_est = np.sqrt(np.linalg.det(tform.coefs[1:, :]))
+
+        e = self.__estim_name__
+        sol = dict()
+        sol[f"{e}_zoom"] = zoom_est
+        sol[f"{e}_theta"] = theta_est
+        sol[f"{e}_dx"] = transl_est[0]
+        sol[f"{e}_dy"] = transl_est[1]
+        sol[f"{e}_time-clq"] = float(tm["end"] - tm["mid"])
+        sol[f"{e}_time-graph"] = float(tm["mid"] - tm["start"])
+        return sol
